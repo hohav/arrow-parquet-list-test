3f66c2b9c70c6e6529ad48a79a9b38fb9a7f7911
melkor <hohav@fastmail.com>
2021-06-27 17:17:27 -0700

    boil it down to just a `ListArray` with some empty items

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,24 @@
-use std::{
-	error::Error,
-	fs::File,
-	sync::Arc,
-};
+use std::{error::Error, fs::File, sync::Arc};
 
 use ::arrow::{
-	array::{Array, ArrayData, ArrayRef, ListArray, PrimitiveArray},
-	buffer::Buffer,
-	datatypes::{DataType, Field, Int32Type},
+	array::{ArrayRef, Int32Builder, ListBuilder},
 	record_batch::RecordBatch,
 };
 
 use parquet::arrow::arrow_writer::ArrowWriter;
 
 pub fn main() -> Result<(), Box<dyn Error>> {
-	// make an array like this:
-	// [[1, 2, 3], [], [4, 5]]
-	let values = PrimitiveArray::<Int32Type>::from_iter_values(vec![1, 2, 3, 4, 5]);
-	let list_data_type = DataType::List(Box::new(Field::new("value", DataType::Int32, false)));
-	let offsets = Buffer::from_slice_ref(&vec![0i32, 3i32, 3i32, 5i32]);
+	let mut builder = ListBuilder::new(Int32Builder::new(10));
+	builder.values().append_value(1)?;
+	builder.append(true)?;
+	builder.append(true)?;
+	builder.values().append_value(2)?;
+	builder.append(true)?;
 
-	let list_data = ArrayData::builder(list_data_type.clone())
-		.len(3)
-		.add_buffer(offsets)
-		.add_child_data(values.data().clone())
-		.build();
-
-	let values = ListArray::from(list_data);
-	for i in 0 .. 3usize {
-		println!("values[{}]: {:?}", i, values.value(i));
-	}
+	let values = builder.finish();
 
 	let batch = RecordBatch::try_from_iter_with_nullable(
-		vec![("values", Arc::new(values) as ArrayRef, false)]
+		vec![("values", Arc::new(values) as ArrayRef, true)]
 	)?;
 
 	let buf = File::create("test.parquet")?;
