30746985b7662364dfaa952a92d93f55ea026f10
melkor <hohav@fastmail.com>
2021-10-09 16:52:22 -0700

    wrap in a `StructArray`

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,31 @@
 use std::{error::Error, fs::File, sync::Arc};
 
 use ::arrow::{
-	array::{ArrayRef, Int32Builder, ListBuilder},
+	array::{ArrayRef, Int32Builder, ListBuilder, StructBuilder},
+	datatypes::{DataType, Field},
 	record_batch::RecordBatch,
 };
 
 use parquet::arrow::arrow_writer::ArrowWriter;
 
-pub fn main() -> Result<(), Box<dyn Error>> {
-	let mut builder = ListBuilder::new(Int32Builder::new(10));
-	builder.values().append_value(1)?;
-	builder.append(true)?;
-	builder.values().append_value(2)?;
-	builder.append(true)?;
+fn list_builder(sb: &mut StructBuilder) -> &mut ListBuilder<Int32Builder> {
+	sb.field_builder(0).unwrap()
+}
 
-	let values = builder.finish();
+pub fn main() -> Result<(), Box<dyn Error>> {
+	let list_type = DataType::List(Box::new(Field::new("id", DataType::Int32, false)));
+	let mut sb = StructBuilder::new(
+		vec![Field::new("items", list_type, false)],
+		vec![Box::new(ListBuilder::new(Int32Builder::new(10)))],
+	);
+
+	list_builder(&mut sb).values().append_value(1)?;
+	list_builder(&mut sb).append(true)?;
+	sb.append(true)?;
+	list_builder(&mut sb).append(true)?;
+	sb.append(true)?;
+
+	let values = sb.finish();
 
 	let batch = RecordBatch::try_from_iter_with_nullable(
 		vec![("values", Arc::new(values) as ArrayRef, false)]
